9a440a7bdb5de35813ead0eb3ef53dd3987bc777
UmarSZN <omian3@gmail.com>
2024-01-14 19:57:06 -0600

    random testing

--- src/script.js
+++ src/script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const h1 = document.querySelector("h1");
+h1.addEventListener("mouseenter", () => {
+  h1.style.backgroundColor = "#f44336";
+});
+
+h1.addEventListener("mouseleave", () => {
+  h1.style.backgroundColor = "#ffffff";
+});
