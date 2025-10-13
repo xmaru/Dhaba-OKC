c058833e08cade11acb3632b870a1932df54e3d2
Umar M <omian3@gmail.com>
2025-10-12 22:47:32 -0500

    fixed footer contrast

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 "use strict";
 
+// ////////////////////////
+// Dark Mode Toggle Logic //
+// ////////////////////////
 const toggleBtn = document.getElementById("modeToggle");
 
 // Check saved preference
@@ -30,15 +33,20 @@ window.addEventListener("scroll", () => {
   }
 });
 
-// // Smooth scrolling for anchor links
-// document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-//   anchor.addEventListener("click", function (e) {
-//     e.preventDefault();
-//     document.querySelector(this.getAttribute("href")).scrollIntoView({
-//       behavior: "smooth",
-//     });
-//   });
-// });
+// ////////////////////////
+// Navigation Code     //
+////////////////////////
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute("href").substring(1);
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
 
 // Hamburger menu toggle
 const hamburger = document.getElementById("hamburger");
